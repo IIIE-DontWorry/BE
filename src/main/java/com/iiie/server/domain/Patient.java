e704da1fd39d60b396766123bb2d8ee00267fbab
Limwngur <ovg07047@naver.com>
2024-11-11 23:25:44 +0900

    feat: medicationCheckList와 양방향 연관관계 설정 이유: 환자 엔티티로 그래프 탐색하여 투약리스트 조회할 케이스가 존재

--- src/main/java/com/iiie/server/domain/Patient.java
+++ src/main/java/com/iiie/server/domain/Patient.java
@@ -1,10 +1,14 @@
 package com.iiie.server.domain;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 
 @Entity
@@ -30,4 +34,8 @@ public class Patient {
 
   @Column(nullable = false)
   private Long kakaoId;
+
+  // ===연관관계===//
+  @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<MedicationCheckList> medicationCheckLists = new ArrayList<>();
 }
