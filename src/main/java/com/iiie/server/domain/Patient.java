bee94cf2d3d893f01e986907dbc7362409dbc605
Limwngur <ovg07047@naver.com>
2024-11-17 21:23:22 +0900

    chore: google포맷팅

--- src/main/java/com/iiie/server/domain/Patient.java
+++ src/main/java/com/iiie/server/domain/Patient.java
@@ -7,14 +7,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import jakarta.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Builder
